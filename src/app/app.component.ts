6c017aa1be3842fe40aa1cac2b559477a6b09015
EliezerSan275 <pesandovalm@dind.mx>
2024-07-15 16:16:27 -0600

    creacion de encabezado requisicion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,23 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderFromatComponent } from "./header-fromat/header-fromat.component";
 import { HeaderPrincipalComponent } from "./header-principal/header-principal.component";
-<<<<<<< Updated upstream
-=======
-import { MenuInicioComponent } from "./menu-inicio/menu-inicio.component";
-import { MenuInicioPrincipalComponent } from './menu-inicio-principal/menu-inicio-principal.component';
-import { CommonModule } from '@angular/common';
 import { HeaderFormatComponent } from "./header-format/header-format.component";
->>>>>>> Stashed changes
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< Updated upstream
-  imports: [RouterOutlet, HeaderFromatComponent, HeaderPrincipalComponent],
-=======
-  imports: [RouterOutlet, HeaderFromatComponent, HeaderPrincipalComponent, MenuInicioComponent,
-    MenuInicioPrincipalComponent, CommonModule, HeaderFormatComponent],
->>>>>>> Stashed changes
+  imports: [RouterOutlet, HeaderFromatComponent, HeaderPrincipalComponent, HeaderFormatComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
