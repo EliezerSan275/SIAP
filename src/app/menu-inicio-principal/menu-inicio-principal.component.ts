960cc0b1392526be1e0d657539c0dd55474daf62
EliezerSan275 <pesandovalm@dind.mx>
2024-07-15 13:54:31 -0600

    Se termino la creacion de menu

--- src/app/menu-inicio-principal/menu-inicio-principal.component.ts
+++ src/app/menu-inicio-principal/menu-inicio-principal.component.ts
@@ -14,16 +14,27 @@ export class MenuInicioPrincipalComponent {
   mostrarSubMenu: boolean = false;
 
   @Input() menuPrin!: String;
-  @Input() subMenu: String[] = [];
+  @Input() subMenuArray: String[] = [];
 
   constructor() {
-    if (this.menuPrin === 'Tablero Financiero')
-      this.subMenu = ['Opcion 1', 'Opcion 2'];
-    if (this.menuPrin === 'Cheques') this.subMenu = ['Opcion 3', 'Opcion 4'];
+    if (this.menuPrin === 'Cheques')
+      this.subMenuArray = ['Opcion 3', 'Opcion 4'];
+    if (this.menuPrin === 'Cuentas por Pagar')
+      this.subMenuArray = ['Opcion 1', 'Opcion 2'];
+    if (this.menuPrin === 'Cheques')
+      this.subMenuArray = ['Opcion 3', 'Opcion 4'];
   }
 
   mostrarMenu() {
     this.mostrarSubMenu = true;
+    if (this.menuPrin === 'Tablero Financiero')
+      this.subMenuArray = ['Opcion 1', 'Opcion 2'];
+    if (this.menuPrin === 'Cheques')
+      this.subMenuArray = ['Opcion 3', 'Opcion 4'];
+    if (this.menuPrin === 'Cuentas por Pagar')
+      this.subMenuArray = ['Opcion 5', 'Opcion 6'];
+    if (this.menuPrin === 'Cuentas por Cobrar')
+      this.subMenuArray = ['Opcion 7', 'Opcion 8'];
   }
 
   ocultarMenu() {
