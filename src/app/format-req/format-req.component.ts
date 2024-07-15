a861d6ec60abf082173aa150366a90208d267839
EliezerSan275 <pesandovalm@dind.mx>
2024-07-15 16:10:50 -0600

    creacion de encabezado requisicion

---
+++ src/app/format-req/format-req.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-format-req',
+  standalone: true,
+  imports: [],
+  templateUrl: './format-req.component.html',
+  styleUrl: './format-req.component.css'
+})
+export class FormatREQComponent {
+
+}
